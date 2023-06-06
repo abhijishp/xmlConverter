0ad40de0ce8b10d5187a34393026e62cfcd9ba8a
Sushma Jayaram <Sushma.jayaramu@ibo.org>
2023-06-06 12:14:17 +0100

    changes to include zip and unzip utilites

--- src/main/resources/jats2odf/dopipe.bat
+++ src/main/resources/jats2odf/dopipe.bat
@@ -2,10 +2,8 @@
 SET VAR=%cd%
 del odf.odt
 del /S /Q \jats2odf\jatstemp
-copy jats2odf\unzip.exe \jats2odf
-copy jats2odf\zip.exe \jats2odf
-call jats2odf/xprocc.bat -i file:%1 jats2odf/pipeline/jats2odf-flat.xpl unzip-invoke=jats2odf\unzip work-dir=file:/C:/jats2odf/jatstemp jats-doc-sysid=foo
+call jats2odf/xprocc.bat -i file:%1 jats2odf/pipeline/jats2odf-flat.xpl unzip-invoke=%VAR%\jats2odf\unzip work-dir=file:/C:/jats2odf/jatstemp jats-doc-sysid=foo
 cd \jats2odf\jatstemp\out
-\jats2odf\zip -0 -X odf.odt jats2odf/pipeline/jatstemp/out/mimetype
-\jats2odf\zip -r odf.odt * -x jats2odf/pipeline/jatstemp/out/mimetype
+%VAR%\jats2odf\zip -0 -X odf.odt mimetype
+%VAR%\jats2odf\zip -r odf.odt * -x mimetype
 copy \jats2odf\jatstemp\out\odf.odt %VAR%\jats2odf
